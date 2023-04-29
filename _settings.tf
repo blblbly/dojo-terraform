68c9a8045df9ccefb487c63d6532c2e0362591ea
blblbly <lilac@padok.fr>
2023-04-29 13:24:37 +0000

    Modification settings

--- _settings.tf
+++ _settings.tf
@@ -8,14 +8,15 @@ terraform {
 }
 
 # Configure the AWS Provider
-provider "aws" {
-        region = "eu-west-3"
-}
 
-default_tags {
-    tags = 
+provider "aws" {
+  region  = "eu-west-3"
+  default_tags {
+    tags = {
         ManagedByTF = "true"
         User = "<blblbly>"
+    }
+  }
 }
 
 # Configure the route 53
