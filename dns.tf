96f2226c6c0c042867a0f1341cec6d888352fbca
blblbly <lilac@padok.fr>
2023-04-29 14:29:47 +0000

    variables

--- dns.tf
+++ dns.tf
@@ -1,11 +1,3 @@
-data "aws_route53_zone" "www" {
-  name = www.zone_name
-}
-
-
-data "aws_lb" "dojo" {
-  name = "padok-dojo-lb"
-}
 
 resource "aws_route53_record" "www" {
   zone_id  = data.aws_route53_zone.www.Z0951853F6JFBYEVJC2P
