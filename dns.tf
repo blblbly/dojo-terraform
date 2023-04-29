323a958f6f2b63615efe546380ed97564d65833d
blblbly <lilac@padok.fr>
2023-04-29 13:59:23 +0000

    commit

--- dns.tf
+++ dns.tf
@@ -1,6 +1,6 @@
 resource "aws_route53_record" "www" {
   zone_id = Z0951853F6JFBYEVJC2P
-  name    = "blblbly-app.dojo.padok.school"
+  name    = "blblblyapp.dojo.padok.school"
   type    = "CNAME"
   records = [padok-dojo-lb-1534647425.eu-west-3.elb.amazonaws.com]
 }
