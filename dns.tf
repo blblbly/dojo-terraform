00be3592ac5548a9b96ba4faf0dd4cdcae1f5b19
blblbly <lilac@padok.fr>
2023-04-29 13:44:28 +0000

    dns

--- dns.tf
+++ dns.tf
@@ -1,6 +1,5 @@
-resource "aws_route53_record" "dojo.padok.school" {
+resource "aws_route53_record" "primary" {
   name    = "blblbly-app.dojo.padok.school"
   type    = "CNAME"
-  records = [padok-dojo-lb-1534647425.eu-west-3.elb.amazonaws.com
-lb_listner_arn = arn:aws:elasticloadbalancing:eu-west-3:450568479740:listener/app/padok-dojo-lb/a842707af71bc2a5/6ae3b87f31e537fa]
+  records = [padok-dojo-lb-1534647425.eu-west-3.elb.amazonaws.com]
 }
