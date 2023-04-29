30fc63ab35bff8aa9913a18b3ab92479f8a657c3
blblbly <lilac@padok.fr>
2023-04-29 15:03:09 +0000

    delete

--- dns.tf
+++
@@ -1,7 +0,0 @@
-
-resource "aws_route53_record" "www" {
-  zone_id  = data.aws_route53_zone.www.Z0951853F6JFBYEVJC2P
-  name     = "www.blblbly-app.dojo.padok.school"
-  type    = "CNAME"
-  records = [padok-dojo-lb-1534647425.eu-west-3.elb.amazonaws.com]
-}
