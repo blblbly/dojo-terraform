5db2b227ef89d296e52611a0452f6c5fdb2f23c0
blblbly <lilac@padok.fr>
2023-04-29 14:04:21 +0000

    dns

--- dns.tf
+++ dns.tf
@@ -1,6 +1,6 @@
-resource "aws_route53_record" "www" {
-  zone_id = Z0951853F6JFBYEVJC2P
-  name    = "blblblyapp.dojo.padok.school"
+resource "aws_route53_record" "this" {
+  zone_id  = data.aws_route53_zone.this.Z0951853F6JFBYEVJC2P
+  name     = $blblbly-app.dojo.padok.school
   type    = "CNAME"
   records = [padok-dojo-lb-1534647425.eu-west-3.elb.amazonaws.com]
 }
